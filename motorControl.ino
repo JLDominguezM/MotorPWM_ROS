ae9a2afe089a2a0b5fbedf99a7f05ef4422ea394
JLDominguezM <A01285873@tec.mx>
2026-03-04 18:02:01 -0600

    Refactor comments and clean up code in motorControl.ino for clarity and consistency

--- motorControl.ino
+++ motorControl.ino
@@ -6,14 +6,12 @@
 #include <rclc/executor.h>
 #include <std_msgs/msg/float32.h>
 
-// Pines indicados en la presentación del reto
 #define IN1_PIN 25
 #define IN2_PIN 27
 #define EN_PIN 26
 
-// Configuración de la señal PWM (frecuencia y resolución)
 const int freq = 980;
-const int resolution = 8; // 8 bits = rango de 0 a 255
+const int resolution = 8;
 
 rcl_subscription_t subscriber;
 std_msgs__msg__Float32 msg;
@@ -31,20 +29,16 @@ void error_loop(){
   }
 }
 
-// Esta función se ejecuta cada vez que llega un mensaje de ROS 2 en /cmd_pwm
 void subscription_callback(const void * msgin)
 {
   const std_msgs__msg__Float32 * msg = (const std_msgs__msg__Float32 *)msgin;
   float cmd = msg->data;
 
-  // 1. Limitar (saturar) el comando al rango [-1.0, 1.0] por seguridad
   if (cmd > 1.0) cmd = 1.0;
   if (cmd < -1.0) cmd = -1.0;
 
-  // 2. Mapear la velocidad (valor absoluto) de 0.0 - 1.0 a PWM de 0 - 255
   int duty_cycle = (int)(abs(cmd) * 255.0);
 
-  // 3. Determinar la dirección de giro con los pines IN1 e IN2
   if (cmd > 0.0) {
     // Giro hacia adelante
     digitalWrite(IN1_PIN, HIGH);
@@ -59,43 +53,35 @@ void subscription_callback(const void * msgin)
     digitalWrite(IN2_PIN, LOW);
   }
 
-  // 4. Mandar la potencia al pin Enable (Nueva sintaxis ESP32 Core v3)
   ledcWrite(EN_PIN, duty_cycle);
 }
 
 void setup() {
   set_microros_transports();
 
-  // Configurar pines de dirección como salida
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
 
-  // Configurar el pin de Enable con PWM (Nueva sintaxis ESP32 Core v3)
   ledcAttach(EN_PIN, freq, resolution);
 
-  // Asegurar que el motor arranque apagado
   digitalWrite(IN1_PIN, LOW);
   digitalWrite(IN2_PIN, LOW);
   ledcWrite(EN_PIN, 0);
 
-  delay(2000); // Darle un respiro al ESP32 para conectar el USB
+  delay(2000);
 
   allocator = rcl_get_default_allocator();
 
-  // Inicializar micro-ROS
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // Crear el nodo llamado "motor"
   RCCHECK(rclc_node_init_default(&node, "motor", "", &support));
 
-  // Crear la suscripción al tópico "/cmd_pwm"
   RCCHECK(rclc_subscription_init_default(
     &subscriber,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
     "/cmd_pwm"));
 
-  // Inicializar el ejecutor para escuchar los callbacks
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 }
